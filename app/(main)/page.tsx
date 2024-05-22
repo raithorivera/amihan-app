12ce58ffa452909532630497b9b96470ad51a44e
raithorivera <raithorivera@gmail.com>
2024-05-22 13:39:00 +0800

    feat: add logo in sidenav

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,8 +1,12 @@
+import Image from 'next/image';
+
 export default function HomePage() {
   return (
     <div>
       <div className='fixed top-0 flex w-16 flex-none items-center whitespace-nowrap py-12 text-sm leading-7 [writing-mode:vertical-rl] bg-white h-screen border-x border-slate-200'>
-        <span className='font-mono text-slate-500'>Amihan</span>
+        <div className='w-[100px] mt-[20px] mb-[36px]'>
+          <Image src='/images/logo/amihan.png' width={180} height={120} alt='Amihan Logo' className='rotate-90' />
+        </div>
         <span className='mt-6 flex gap-6 font-bold text-slate-900'>
           <span aria-hidden='true' className='text-slate-400'>
             /
@@ -12,7 +16,9 @@ export default function HomePage() {
       </div>
 
       <div className='ml-16 relative'>
-        <header className='bg-slate-50 lg:fixed lg:inset-y-0 lg:left-16 lg:flex lg:w-112 lg:items-start lg:overflow-y-auto xl:w-120'>LEFT</header>
+        <header className='bg-slate-50 lg:fixed lg:inset-y-0 lg:left-16 lg:flex lg:w-112 lg:items-start lg:overflow-y-auto xl:w-120 px-4 py-4'>
+
+        </header>
 
         <main className='border-t border-slate-200 lg:relative lg:mb-28 lg:ml-112 lg:border-t-0 xl:ml-120'>
           <div className='relative'>RIGHT</div>
