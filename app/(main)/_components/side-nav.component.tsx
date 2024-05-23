518019da019b231c864cb3411c7dc8e90cd7b9fa
raithorivera <raithorivera@gmail.com>
2024-05-23 22:41:50 +0800

    feat: add on-click actions on change themee and language

--- app/(main)/_components/side-nav.component.tsx
+++ app/(main)/_components/side-nav.component.tsx
@@ -2,10 +2,19 @@
 
 import Image from 'next/image';
 import { Button } from '@ui';
+import { toast } from 'sonner';
 
 import { Icon } from '@/components/icon.component';
 
 export default function SideNavComponent() {
+  const onChangeTheme = () => {
+    toast.warning('Change Theme!', { description: 'This feature is currently being developed and will be available in an upcoming release. Stay tuned for updates!' });
+  };
+
+  const onChangeLanguage = () => {
+    toast.warning('Change Language!', { description: 'This feature is currently being developed and will be available in an upcoming release. Stay tuned for updates!' });
+  };
+
   return (
     <div className='fixed top-0 flex w-16 flex-none items-center whitespace-nowrap py-12 text-sm leading-7 [writing-mode:vertical-rl] bg-white h-screen border-x border-slate-200'>
       <div className='w-[100px] mt-[20px] mb-[36px] ml-[6px]'>
@@ -24,11 +33,11 @@ export default function SideNavComponent() {
           <Icon.cog className='h-4 w-4' />
         </Button>
 
-        <Button variant='outline' size='icon'>
+        <Button variant='outline' size='icon' onClick={onChangeTheme}>
           <Icon.palette className='h-4 w-4' />
         </Button>
 
-        <Button variant='outline' size='icon'>
+        <Button variant='outline' size='icon' onClick={onChangeLanguage}>
           <Icon.languages className='h-4 w-4' />
         </Button>
       </div>
