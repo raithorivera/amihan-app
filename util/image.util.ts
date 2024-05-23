344d656066352a3ef17fae5fc097e96e4ddcdbac
raithorivera <raithorivera@gmail.com>
2024-05-23 18:12:12 +0800

    feat: download weather-icons, to prevent error due to rate-limiting

--- util/image.util.ts
+++ util/image.util.ts
@@ -1,6 +1,6 @@
 import { isBlank } from './validation.util';
 
 export const getWeatherIcon = (value: string, size?: string) => {
-  if (value && !isBlank(value)) return `https://openweathermap.org/img/wn/${value}@${size || '4x'}.png`;
-  return '/images/icon_svg/sunrise.svg';
+  if (value && !isBlank(value)) return `/images/weather_icon/${value}@${size || '4x'}.png`;
+  return '/images/weather_icon/03d@4x.png';
 };
