afa172ba7c8a953c1860694b2aaf81ccdaa5a3bc
raithorivera <raithorivera@gmail.com>
2024-05-24 11:21:48 +0800

    chore: add start.sh to run in supervisor using /bin/bash

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Set Node.js and npm paths directly
+export PATH="/root/.nvm/versions/node/v18.20.3/bin:$PATH"
+
+# Navigate to the project directory
+cd /projects/amihan-app
+
+# Run the npm start command
+/root/.nvm/versions/node/v18.20.3/bin/npm start
